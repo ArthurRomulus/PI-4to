990d04739f8000127a93f5db00b43217c98af2e1
ArthurRomulus <miguepg06@gmail.com>
2026-02-25 14:00:34 -0600

    Update 2

--- user_interface/main/main.py
+++ user_interface/main/main.py
@@ -1,7 +1,7 @@
-import sys;
-
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton;
+import sys, cv2 as cv;
+from PyQt6.QtCore import QSize, Qt, QTimer
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QLabel;
+from PyQt6.QtGui import QImage, QPixmap
 
 buttons = {
     "login" : {
@@ -10,8 +10,6 @@ buttons = {
     },
     
 }
-
-
 
 input = {
     "username" : {
@@ -28,9 +26,37 @@ input = {
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__();
+
+        cap = cv.VideoCapture(0)
+        
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout);
+        self.cameralabel = QLabel();
+        self.cameralabel.setAlignment(Qt.AlignmentFlag.AlignHCenter);
+        self.cameralabel.setFixedSize(400, 500);
+        self.layout.addWidget(self.cameralabel);
+        self.cap  = cv.VideoCapture(0);
+
+        self.timer = QTimer();
+
+        def update_frame(self):
+            ret, frame = self.cap.read();
+            if ret:
+                frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB);
+                h,w,ch = frame.shape;
+                bpl = ch * w;
+
+                qt_image = QImage(frame.data, w,h, bpl, QImage.Format.Format_RGB888)
+
+                self.cameralabel.setPixmap(QPixmap.fromImage(qt_image))
+        
+
+        self.timer.timeout.connect(update_frame(self));
+        self.timer.start(30);
+
         self.setWindowTitle("My app");
         button = QPushButton("Press me");
-        button.setFixedSize(200, 100)
+        button.setFixedSize(100, 50)
         button.setStyleSheet("""
             QPushButton {
                 background-color: rgb(255,255,255);
@@ -38,13 +64,14 @@ class MainWindow(QMainWindow):
                 border-color: rgb(45, 45, 45);
                 border-radius: 10px;
                 padding: 10px;
-                font-size: 16px;
+                font-size: 11px;
             }
             QPushButton:hover {
                 background-color: rgb(235, 235, 235);
                 border-color: rgb(45, 45, 45);
             }
         """)
+        
         self.setFixedSize(QSize(1000, 600))
         self.setCentralWidget(button)
 
