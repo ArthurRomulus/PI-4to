9d18a0433724180ef349fd0a569db1a8b2c4d095
ArthurRomulus <miguepg06@gmail.com>
2026-02-25 10:05:28 -0600

    update 23

--- user_interface/main/main.py
+++ user_interface/main/main.py
@@ -3,7 +3,27 @@ import sys;
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton;
 
+buttons = {
+    "login" : {
+        "name" : "login",
+        "color" : (255, 255, 255),
+    },
+    
+}
 
+
+
+input = {
+    "username" : {
+        "name" : "username",
+        "type" : "text",
+    },
+
+    "password" : {
+        "name" : "password",
+        "type" : "passwordtext",
+    }
+}
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -11,6 +31,20 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("My app");
         button = QPushButton("Press me");
         button.setFixedSize(200, 100)
+        button.setStyleSheet("""
+            QPushButton {
+                background-color: rgb(255,255,255);
+                color: rgb(45, 45, 45);
+                border-color: rgb(45, 45, 45);
+                border-radius: 10px;
+                padding: 10px;
+                font-size: 16px;
+            }
+            QPushButton:hover {
+                background-color: rgb(235, 235, 235);
+                border-color: rgb(45, 45, 45);
+            }
+        """)
         self.setFixedSize(QSize(1000, 600))
         self.setCentralWidget(button)
 
