e8480020a5368f253418e743d1e11daaf23d2f99
ArthurRomulus <miguepg06@gmail.com>
2026-03-20 18:28:59 -0600

    update 7

--- MainAdmin.py
+++ MainAdmin.py
@@ -1,9 +1,9 @@
-from ui.admin.userlist import userlist;
+from ui.admin.auditlogs import PantallaHistorial;
 
 from PyQt5.QtWidgets import QApplication; import sys;
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = userlist(admin_id=1)
+    window = PantallaHistorial(admin_id=1)
     window.show()
     sys.exit(app.exec_())
