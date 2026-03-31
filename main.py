80bd0287ac8e69c27ea19b3c9912f5613a820512
ArthurRomulus <miguepg06@gmail.com>
2026-03-31 12:13:08 -0600

    cf 2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import QApplication
 from ui.admin.userlist import userlist
 from database.consultas import crear_tablas, limpiar_embeddings_invalidos
 
-
+print ("hi")
 if __name__ == "__main__":
     crear_tablas()
     limpiar_embeddings_invalidos()
