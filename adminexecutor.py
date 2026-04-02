f0b11057598674eeb3a65878d25a890ca202a244
ArthurRomulus <miguepg06@gmail.com>
2026-04-02 12:31:08 -0600

    librerias parte 3

--- adminexecutor.py
+++ adminexecutor.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from ui.admin.admin_panel import AdminPanelWindow
 from database.consultas import crear_tablas, limpiar_embeddings_invalidos
-
+print("1w")
 if __name__ == "__main__":
     crear_tablas()
     limpiar_embeddings_invalidos()
