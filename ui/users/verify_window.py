21a294613e1fcd75956c571bb0ff08233d600824
ArthurRomulus <miguepg06@gmail.com>
2026-05-27 22:34:36 -0600

    3

--- ui/users/verify_window.py
+++ ui/users/verify_window.py
@@ -625,9 +625,11 @@ class VerifyWindow(QWidget):
             self._countdown_timer.stop()
         self._stop_inactivity_timer()
         self._stop_camera()
-        if self.main_window:
-            self.main_window.show()
-        self.close()
+        self.countdown_label.setVisible(False)
+        if self._result_banner:
+            self._result_banner.deleteLater()
+            self._result_banner = None
+        self._reintentar()
 
     def _stop_camera(self):
         if self.camera_thread and self.camera_thread.isRunning():
